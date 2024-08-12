4fa0a090f9edefe15eb14f882e05ea425eca6c01
jenniferchristine <jeja2306@student.miun.se>
2024-08-13 00:40:27 +0200

    added btn to get to top page again

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     displayData();
 
+    const toTopBtn = document.getElementById("topBtn");
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            toTopBtn.style.display = 'block';
+        } else {
+            toTopBtn.style.display = 'none';
+        }
+    });
+
+    toTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    });
+
     const addBtn = document.getElementById("confirmBtn");
 
     if (addBtn) {
