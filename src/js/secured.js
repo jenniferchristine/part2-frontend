665008d08a8d252be5bbfd7e0d2370530eccbd3a
jenniferchristine <jeja2306@student.miun.se>
2024-08-14 02:17:21 +0200

    finished code for log in and log out

---
+++ src/js/secured.js
@@ -0,0 +1,42 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutBtn = document.getElementById("logout-btn");
+
+    logoutBtn.addEventListener('click', (e) => {
+        e.preventDefault(); logOut();
+    });
+
+    authorizeUser();
+});
+
+async function authorizeUser() {
+    try { 
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+            localStorage.setItem("redirected", "true");
+            throw new Error("Failed - Token missing");
+        }
+
+        // kontrollera skyddad resurs
+        const response = await fetch("https://pastaplace.onrender.com/protected", {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${token}` }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Something went wrong while accessing protected resource: ${response.statusText}`);
+        }
+        console.log("You are now on protected route");
+
+    } catch (error) {
+        console.error("Something went wrong while accessing protected resource:", error);
+        window.location.href = "index.html"
+    }
+}
+
+function logOut() {
+    localStorage.removeItem("token");
+    window.location.href ="index.html"
+};
